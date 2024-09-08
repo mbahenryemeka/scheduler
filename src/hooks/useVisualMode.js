0de42a7a3ad99dd75b2678d43fedbb2d8f511106
Henry <mbahenryemeka@gmail.com>
2024-09-08 07:15:09 -0400

    implement limit

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,28 @@
 import { useState } from "react";
-export default function useVisualMode(initial) {
-  const [history, setHistory] = useState([initial]); 
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode])
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => {
+        const newHistory = [...prev];
+        newHistory[newHistory.length - 1] = newMode;
+        return newHistory;
+      });
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    setHistory(prev => {
+      if (prev.length > 1) {
+        return prev.slice(0, prev.length - 1);
+      }
+      return prev;
+    });
   }
 
-  return { mode: history[history.length -1], transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
